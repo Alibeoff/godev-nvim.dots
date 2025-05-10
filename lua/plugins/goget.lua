543a9cca0f5efe143111dc86458c408a7e11cebb
vooyage <evgeniys0607@gmail.com>
2025-05-10 22:33:29 +0300

    fix go-get tui

--- lua/plugins/goget.lua
+++ lua/plugins/goget.lua
@@ -1,13 +1,17 @@
 return {
   "https://github.com/RomanVolkov/go.get.nvim",
-  dependencies = { "nvim-telescope/telescope.nvim" },
-  keys = {
-    {
-      "sd",
-      function()
-        require("telescope").extensions.go_get.packages_search()
-      end,
-      desc = "[G]o [G]et packages"
-    },
-  }
+  dependencies = {
+    "nvim-telescope/telescope.nvim",
+  },
+  keys = function()
+    return {
+      {
+        "sd",
+        function()
+          require("telescope").extensions.go_get.packages_search()
+        end,
+        desc = "[G]o [G]et packages",
+      },
+    }
+  end,
 }
