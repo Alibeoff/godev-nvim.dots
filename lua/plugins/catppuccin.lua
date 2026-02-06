b9a097ee15a514f396d8ecab643ea8ed577cb0a4
vooyage <evgeniys0607@gmail.com>
2026-02-06 16:06:14 +0300

    fix: telescope.nvim

--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,13 +3,13 @@ return {
   name = "catppuccin",
   priority = 1000,
   lazy = false,
-  
+
   -- Опции для настройки Catppuccin
   opts = {
     flavour = "mocha", -- Указываем вариант темы
     transparent_background = true,
     show_end_of_buffer = false,
-    
+
     -- Интеграции с плагинами
     integrations = {
       cmp = true,
@@ -50,7 +50,7 @@ return {
         },
       },
     },
-    
+
     -- Дополнительные стили
     styles = {
       comments = { "italic" },
@@ -66,56 +66,56 @@ return {
       types = {},
       operators = {},
     },
-    
+
     -- Кастомные хайлайты
     custom_highlights = function(colors)
       return {
         -- Кастомные бордеры
         FloatBorder = { fg = colors.blue },
-        TelescopeBorder = { fg = colors.blue },
-        TelescopePromptBorder = { fg = colors.blue },
-        TelescopeResultsBorder = { fg = colors.blue },
-        TelescopePreviewBorder = { fg = colors.blue },
+        TelescopeBorder = { bg = "NONE", fg = colors.blue },
+        TelescopePromptBorder = { bg = "NONE", fg = colors.blue },
+        TelescopeResultsBorder = { bg = "NONE", fg = colors.blue},
+        TelescopePreviewBorder = { bg = "NONE", fg = colors.blue},
         DiagnosticFloatingBorder = { fg = colors.blue },
         LspInfoBorder = { fg = colors.blue },
-        
+
         -- Кастомные фоны для плавающих окон
-        TelescopeNormal = { bg = colors.mantle },
-        TelescopePromptNormal = { bg = colors.mantle },
-        TelescopeResultsNormal = { bg = colors.mantle },
-        TelescopePreviewNormal = { bg = colors.mantle },
-        
+        TelescopeNormal = { bg = "NONE" },
+        TelescopePromptNormal = { bg = "NONE" },
+        TelescopeResultsNormal = { bg = "NONE" },
+        TelescopePreviewNormal = { bg = "NONE" },
+
         -- Лучшие настройки для прозрачности
         NormalFloat = { bg = "none" },
         FloatTitle = { bg = "none" },
         FloatFooter = { bg = "none" },
-        
+
         -- Разделители окон
         WinSeparator = { fg = colors.surface2 },
         VertSplit = { fg = colors.surface2 },
-        
+
         -- Строка статуса
         StatusLine = { bg = "none" },
         StatusLineNC = { bg = "none" },
-        
+
         -- Номера строк
         LineNr = { fg = colors.overlay0 },
         CursorLineNr = { fg = colors.lavender },
-        
+
         -- Символы в конце буфера
         EndOfBuffer = { fg = colors.base },
       }
     end,
   },
-  
+
   -- Конфигурация
   config = function(_, opts)
     -- 1. Сначала настраиваем Catppuccin
     require("catppuccin").setup(opts)
-    
+
     -- 2. Устанавливаем цветовую схему
     vim.cmd.colorscheme("catppuccin")
-    
+
     -- 3. После загрузки темы применяем дополнительные настройки прозрачности
     vim.schedule(function()
       -- Основные группы для полной прозрачности
@@ -131,41 +131,41 @@ return {
         "NotifyERRORBorder", "NvimTreeNormal", "NvimTreeNormalNC",
         "NvimTreeEndOfBuffer", "MasonNormal", "WhichKeyFloat",
       }
-      
+
       for _, group in ipairs(transparent_groups) do
         local hl = vim.api.nvim_get_hl(0, { name = group, link = false })
         hl.bg = hl.bg and "none" or nil
         vim.api.nvim_set_hl(0, group, hl)
       end
-      
+
       -- Специальные настройки для Pmenu
       vim.api.nvim_set_hl(0, "PmenuSel", {
         bg = opts.integrations.native_lsp.enabled and "#585b70" or "#313244",
         fg = "none",
       })
-      
+
       -- Убедимся, что Treesitter использует наши цвета
       if opts.integrations.treesitter then
         vim.api.nvim_set_hl(0, "@variable", { fg = opts.flavour == "mocha" and "#cdd6f4" or "#4c4f69" })
         vim.api.nvim_set_hl(0, "@function", { fg = opts.flavour == "mocha" and "#89b4fa" or "#1e66f5" })
         vim.api.nvim_set_hl(0, "@keyword", { fg = opts.flavour == "mocha" and "#cba6f7" or "#8839ef" })
       end
-      
+
       -- Для Go-разработки - дополнительные хайлайты
       vim.api.nvim_create_autocmd("FileType", {
         pattern = "go",
         callback = function()
-          vim.treesitter.start() 
+          vim.treesitter.start()
         end,
       })
     end)
   end,
-  
+
   -- Инициализация перед загрузкой
   init = function()
     -- Устанавливаем глобальные переменные ДО загрузки плагина
     vim.g.catppuccin_flavour = "mocha"
-    
+
     -- Предотвращаем мигание при загрузке
     vim.api.nvim_create_autocmd("VimEnter", {
       once = true,
