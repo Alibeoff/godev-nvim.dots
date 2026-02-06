19a625bd31cb3ac379d19e48f44cac091a09da2f
vooyage <evgeniys0607@gmail.com>
2026-02-06 14:51:02 +0300

    Add: Inline errors

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -3,10 +3,18 @@ return {
     "neovim/nvim-lspconfig",
     event = "VeryLazy",
     config = function()
-    vim.keymap.set("n", "sa", function()
-      vim.lsp.buf.code_action({ filters = { include = { "source.addImport", "source.OrganizeImports" }, } })
-    end, { desc = "Go: Organize Imports" })
-  end,
+      -- Дополнительно включить виртуальный текст с отступами
+      vim.diagnostic.config({
+        virtual_text = {
+          prefix = "",
+          spacing = 6, -- табуляция перед текстом ошибки
+        },
+      })
+
+      vim.keymap.set("n", "sa", function()
+        vim.lsp.buf.code_action({ filters = { include = { "source.addImport", "source.OrganizeImports" }, } })
+      end, { desc = "Go: Organize Imports" })
+    end,
 
   },
   {
@@ -14,9 +22,9 @@ return {
     opts = {
       ui = {
         icons = {
-            package_installed = "✓",
-            package_pending = "➜",
-            package_uninstalled = "✗"
+          package_installed = "✓",
+          package_pending = "➜",
+          package_uninstalled = "✗"
         }
       }
     }
@@ -26,10 +34,19 @@ return {
     opts = {
       ensure_installed = { "lua_ls", "gopls", "jsonls", "yamlls", "dockerls", "cssls", "html", "docker_compose_language_service" },
       handlers = {
-        function (server_name)
+        function(server_name)
           require("lspconfig")[server_name].setup({})
         end,
       }
     }
+  },
+  {
+    "folke/trouble.nvim",
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    opts = {
+      -- Отступы в окне trouble
+      padding = false,
+      indent_lines = true,
+    },
   }
 }
